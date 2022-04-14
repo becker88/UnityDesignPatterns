8449ab0317509ca8a86198d2d7eb76f2ef25d249
Samuel Asher Rivello <74617375+srivelloBeamable@users.noreply.github.com>
2022-04-14 17:38:14 +0100

    Update Singleton Demos

--- Unity/Assets/3rdParty/RMC/Demos/Projects/UnityDesignPatterns/Patterns/CreationalPatterns/Singleton/SingletonMonoBehaviour 1/ScriptableSingletonDemo.cs
+++ Unity/Assets/3rdParty/RMC/Demos/Projects/UnityDesignPatterns/Patterns/CreationalPatterns/Singleton/SingletonMonoBehaviour 1/ScriptableSingletonDemo.cs
@@ -6,16 +6,10 @@ namespace RMC.Projects.UnityDesignPatterns.Patterns.CreationalPatterns.Singleton
 	/// Ensure a class has only one instance and provide a global point of access to it.
 	/// See <a href="https://www.dofactory.com/net/singleton-design-pattern">docs</a>.
 	/// </summary>
-	public class SingletonDemo : MonoBehaviour
+	public class ScriptableSingletonDemo : MonoBehaviour
 	{
 		protected void Start()
 		{
-			// Singleton MonoBehaviour
-			// -- Call multiple times. Note: It only gets created 1 time
-			MySingleton.Instance.SayHelloWorld();
-			MySingleton.Instance.SayHelloWorld();
-			MySingleton.Instance.SayHelloWorld();
-			
 			// Singleton ScriptableObject
 			// -- Writes to local path
 			MyScriptableSingleton.instance.Log();
---
+++ Unity/Assets/3rdParty/RMC/Demos/Projects/UnityDesignPatterns/Patterns/CreationalPatterns/Singleton/SingletonMonoBehaviour/SingletonMonoBehaviourDemo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace RMC.Projects.UnityDesignPatterns.Patterns.CreationalPatterns.Singleton
+{
+	/// <summary>
+	/// Ensure a class has only one instance and provide a global point of access to it.
+	/// See <a href="https://www.dofactory.com/net/singleton-design-pattern">docs</a>.
+	/// </summary>
+	public class SingletonMonoBehaviourDemo : MonoBehaviour
+	{
+		protected void Start()
+		{
+			// Singleton MonoBehaviour
+			// -- Call multiple times. Note: It only gets created 1 time
+			MySingleton.Instance.SayHelloWorld();
+			MySingleton.Instance.SayHelloWorld();
+			MySingleton.Instance.SayHelloWorld();
+			
+		}
+	}
+}
